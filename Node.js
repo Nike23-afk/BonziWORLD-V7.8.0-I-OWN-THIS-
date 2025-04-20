4d2162b41c162f1dd4c78a06add5b76a68eb8062
LenDevenCool93 <nikedimoshagos6@gmail.com>
2025-04-19 20:16:57 -0600

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,1 +1,23 @@
+const express = require("express");
+const { exec } = require("child_process");
+const fs = require("fs");
+const app = express();
+const PORT = 4000;
 
+app.get("/tts", (req, res) => {
+  const text = req.query.text || "Hello";
+  const file = `temp.wav`;
+
+  exec(`espeak "${text}" --stdout > ${file}`, (err) => {
+    if (err) {
+      return res.status(500).send("TTS failed");
+    }
+    res.sendFile(__dirname + "/" + file, () => {
+      fs.unlinkSync(file); // delete after sending
+    });
+  });
+});
+
+app.listen(2404, () => {
+  console.log(`eSpeak TTS server running at http://localhost:${2404}`);
+});
